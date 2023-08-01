d5656ac8d7a60733f47ccae66f292f3dee530abe
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2023-08-01 14:47:14 +0800

    补充原子操作 添加超出序列号后等待

--- service/Snowflake.go
+++ service/Snowflake.go
@@ -2,13 +2,12 @@ package service
 
 import (
 	"GoLeaf/config"
-	"sync"
+	"sync/atomic"
 	"time"
 )
 
 // Snowflake 结构体
 type Snowflake struct {
-	mu         sync.Mutex
 	latestTime int64
 	workerID   int64
 	sequenceID int64
@@ -23,30 +22,32 @@ func NewSnowflake(workerID int64) *Snowflake {
 	}
 }
 
-// GenerateID 生成唯一标识符
 func (sf *Snowflake) GenerateID() int64 {
-
 	now := time.Now().UnixNano() / 1e6
 	//todo 时钟回拨处理
 	//if now < sf.latestTime {
 	//	panic("Clock moved backwards, refusing to generate id")
 	//}
 	// 如果当前时间与上次生成ID的时间相同，则进行毫秒内序列
-	if now == sf.latestTime {
-		sf.mu.Lock()
-
-		sf.sequenceID = (sf.sequenceID + 1) & 4095
-		if sf.sequenceID == 0 {
-			for now <= sf.latestTime {
+	if now == atomic.LoadInt64(&sf.latestTime) {
+		for {
+			sequenceID := atomic.LoadInt64(&sf.sequenceID)
+			nextSequenceID := (sequenceID + 1) & 4095
+			// 如果当前毫秒内序列已经用完，则等待下一毫秒
+			if nextSequenceID == 0 {
 				now = time.Now().UnixNano() / 1e6
+				for now <= sf.latestTime {
+					now = time.Now().UnixNano() / 1e6
+				}
+			}
+			if atomic.CompareAndSwapInt64(&sf.sequenceID, sequenceID, nextSequenceID) {
+				break
 			}
 		}
-		sf.mu.Unlock()
 	} else {
-		sf.sequenceID = 0
+		atomic.StoreInt64(&sf.sequenceID, 0)
 	}
-
-	sf.latestTime = now
-	ID := (now-config.StartTime)<<22 | (sf.workerID << 12) | sf.sequenceID
+	atomic.StoreInt64(&sf.latestTime, now)
+	ID := (now-config.StartTime)<<22 | (sf.workerID << 12) | atomic.LoadInt64(&sf.sequenceID)
 	return ID
 }
